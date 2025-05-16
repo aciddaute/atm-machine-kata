b5459d7cf6d414465c52b596ce780425a3ae14e3
Daute Rodríguez <daute@acidtango.com>
2025-05-16 17:08:13 +0100

    refactor: recursive solution

--- src/main.ts
+++ src/main.ts
@@ -25,29 +25,27 @@ const allBills = [
 ]
 
 export class ATM implements ATMMachine {
+  private availableBills: Bill[];
 
-  constructor(private readonly availableBills: Bill[] = allBills) {
-  }
-
-  private withdrawBills(amount: number, bill: Bill): {money?: Money, rest: number} {
-    const quantity = Math.floor(amount / bill.amount)
-    const rest = amount % bill.amount
-    const money = quantity > 0 ? {quantity, denomination: bill.amount} : undefined
-    return {money, rest}
+  constructor(availableBills: Bill[] = allBills) {
+    this.availableBills = availableBills.sort((a, b) => b.amount - a.amount);
   }
 
   withdraw(amount: number): Money[] {
-    const bills: Money[] = []
-    let remainingAmount = amount
-
-    this.availableBills.sort((a, b) => b.amount - a.amount).forEach((bill) => {
-      const {money, rest} = this.withdrawBills(remainingAmount, bill)
-      if (money) {
-        bills.push(money)
-      }
-      remainingAmount = rest
-    })
-
-    return bills
+    return this.withdrawBills(amount, this.availableBills);
   }
+
+  private withdrawBills(amount: number, bills: Bill[]): Money[] {
+    const bill = bills.at(0)
+
+    if (!bill || !amount) return []
+
+    const quantity = Math.floor(amount / bill.amount)
+    const rest = amount % bill.amount
+    const money = quantity > 0 ? {quantity, denomination: bill.amount} : undefined
+
+    if (!money) return this.withdrawBills(rest, bills.slice(1));
+
+    return [money, ... this.withdrawBills(rest, bills.slice(1))]
+    }
 }
